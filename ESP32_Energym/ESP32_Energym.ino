44aa97a2d2bacd1f5402739bda49ddfabcbbe13f
NatuRosen <107327994+Sofia-F3@users.noreply.github.com>
2024-07-30 11:14:34 -0300

    Update

--- ESP32_Energym/ESP32_Energym.ino
+++ ESP32_Energym/ESP32_Energym.ino
@@ -1,106 +1,110 @@
-/*
-  Rui Santos
-  Complete project details at our blog.
-    - ESP32: https://RandomNerdTutorials.com/esp32-firebase-realtime-database/
-    - ESP8266: https://RandomNerdTutorials.com/esp8266-nodemcu-firebase-realtime-database/
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-  Based in the RTDB Basic Example by Firebase-ESP-Client library by mobizt
-  https://github.com/mobizt/Firebase-ESP-Client/blob/main/examples/RTDB/Basic/Basic.ino
-*/
-
 #include <Arduino.h>
 #if defined(ESP32)
-  #include <WiFi.h>
+#include <WiFi.h>
 #elif defined(ESP8266)
-  #include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h>
 #endif
 #include <Firebase_ESP_Client.h>
 
-//Provide the token generation process info.
+// Provide the token generation process info.
 #include "addons/TokenHelper.h"
-//Provide the RTDB payload printing info and other helper functions.
+// Provide the RTDB payload printing info and other helper functions.
 #include "addons/RTDBHelper.h"
 
 // Insert your network credentials
-#define WIFI_SSID "ORT-IoT"
-#define WIFI_PASSWORD "OrtIOTnew22$2"
+#define WIFI_SSID "moto g(60)s_1622"
+#define WIFI_PASSWORD "sofi1234"
 
 // Insert Firebase project API Key
 #define API_KEY "AIzaSyDabWGSqR2t98L11OSxnb8aTBGMmWgsxMY"
 
-// Insert RTDB URLefine the RTDB URL */
-#define DATABASE_URL "https://energym-f9949-default-rtdb.firebaseio.com/" 
+#define DATABASE_URL "https://energym-f9949-default-rtdb.firebaseio.com/"
 
-//Define Firebase Data object
+// Insert Authorized Email and Corresponding Password
+#define USER_EMAIL "sofia2@gmail.com"
+#define USER_PASSWORD "123456"
+
+// Define Firebase objects
 FirebaseData fbdo;
-
 FirebaseAuth auth;
 FirebaseConfig config;
 
+// Variable to save USER UID
+String uid;
+
 unsigned long sendDataPrevMillis = 0;
 int count = 0;
-bool signupOK = false;
 
-void setup(){
-  Serial.begin(115200);
+// Initialize WiFi
+void initWiFi() {
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED){
-    Serial.print(".");
-    delay(300);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
   }
-  Serial.println();
-  Serial.print("Connected with IP: ");
   Serial.println(WiFi.localIP());
   Serial.println();
+}
 
-  /* Assign the api key (required) */
+void setup() {
+  Serial.begin(115200);
+
+  // Initialize WiFi
+  initWiFi();
+
+  // Assign the api key (required)
   config.api_key = API_KEY;
 
   /* Assign the RTDB URL (required) */
   config.database_url = DATABASE_URL;
 
-  /* Sign up */
-  if (Firebase.signUp(&config, &auth, "", "")){
-    Serial.println("ok");
-    signupOK = true;
-  }
-  else{
-    Serial.printf("%s\n", config.signer.signupError.message.c_str());
-  }
+  // Assign the user sign in credentials
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
 
-  /* Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
-  
+  Firebase.reconnectWiFi(true);
+  fbdo.setResponseSize(4096);
+
+  // Assign the callback function for the long running token generation task
+  config.token_status_callback = tokenStatusCallback;  //see addons/TokenHelper.h
+
+  // Assign the maximum retry of token generation
+  config.max_token_generation_retry = 5;
+
+  // Initialize the library with the Firebase authen and config
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
+
+  // Getting the user UID might take a few seconds
+  Serial.println("Getting User UID");
+  while ((auth.token.uid) == "") {
+    Serial.print('.');
+    delay(1000);
+  }
+  // Print user UID
+  uid = auth.token.uid.c_str();
+  Serial.print("User UID: ");
+  Serial.print(uid);
 }
 
-void loop(){
-  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
+void loop() {
+  if (Firebase.isTokenExpired()) {
+    Firebase.refreshToken(&config);
+    Serial.println("Refresh token");
+  }
+
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)) {
     sendDataPrevMillis = millis();
     // Write an Int number on the database path test/int
-    if (Firebase.RTDB.setInt(&fbdo, "test/int", count)){
+    if (Firebase.RTDB.setInt(&fbdo, "users/" + uid + "/Energy", count)) {
       Serial.println("PASSED");
       Serial.println("PATH: " + fbdo.dataPath());
       Serial.println("TYPE: " + fbdo.dataType());
-    }
-    else {
+    } else {
       Serial.println("FAILED");
       Serial.println("REASON: " + fbdo.errorReason());
     }
     count++;
-    
-    // Write an Float number on the database path test/float
-    if (Firebase.RTDB.setFloat(&fbdo, "test/float", 0.01 + random(0,100))){
-      Serial.println("PASSED");
-      Serial.println("PATH: " + fbdo.dataPath());
-      Serial.println("TYPE: " + fbdo.dataType());
-    }
-    else {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + fbdo.errorReason());
-    }
   }
 }
